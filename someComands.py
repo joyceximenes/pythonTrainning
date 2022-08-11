cc6fe3c8469b0f6c2c93715da83c17906e77fb21
Joyce Ximenes <91160284+joyceximenes@users.noreply.github.com>
2022-08-11 20:57:20 -0300

    Create someComands.py

---
+++ someComands.py
@@ -0,0 +1,22 @@
+# escrever comando na tela
+print('Hello world')
+
+#fazendo calculo
+print(5-9)
+
+soma = 6+5
+print('soma = ', soma)
+a = 10
+
+#boolean
+print(a==20)
+
+#if 
+if (soma==11):
+    print('ok')
+else: 
+    print(erro)
+    
+#for 
+    for n in range (5): # incrementa de 1 em 1
+        print(n)
