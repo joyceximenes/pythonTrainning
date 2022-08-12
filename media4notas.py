aa1f3b70cb061b7fbc39956d92d5e28a21c148b1
Joyce Ximenes <91160284+joyceximenes@users.noreply.github.com>
2022-08-11 21:24:32 -0300

    Create media4notas.py

---
+++ media4notas.py
@@ -0,0 +1,9 @@
+# le os numeros
+n1 = int(input('Digite a primeira média: '))
+n2 = int(input('Digite a segunda média: '))
+n3 = int(input('Digite a terceira média: '))
+n4 = int(input('Digite a quarta média: '))
+media =  (n1 + n2 + n3 + n4)/4
+
+#mostra a media
+print('a media é ', media)
