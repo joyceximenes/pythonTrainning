7a1cd702d79d62e6ff7ad822c8a77396d44185f0
Joyce Ximenes <91160284+joyceximenes@users.noreply.github.com>
2022-08-13 11:11:45 -0300

    Create excessoQuilos.py

---
+++ excessoQuilos.py
@@ -0,0 +1,16 @@
+# João Papo-de-Pescador, homem de bem, comprou um microcomputador para controlar o rendimento diário de seu trabalho. 
+# Toda vez que ele traz um peso de peixes maior que o estabelecido pelo regulamento de pesca 
+# do estado de São Paulo (50 quilos) deve pagar uma multa de R$ 4,00 por quilo excedente. 
+# João precisa que você faça um programa que leia a variável peso (peso de peixes) e calcule o excesso.
+# Gravar na variável excesso a quantidade de quilos além do limite e na variável
+# multa o valor da multa que João deverá pagar.
+# Imprima os dados do programa com as mensagens adequadas.
+
+peso = float(input('Digite a quantidade de quilos: '))
+
+if (peso>50):
+    excesso = peso - 50
+    multa = excesso * 4
+
+print('O excesso de quilos foi', excesso)
+print('A multa referente é', multa)
