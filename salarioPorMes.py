c57f634dea023115a7227899d2f9f28bcdaa4b75
Joyce Ximenes <91160284+joyceximenes@users.noreply.github.com>
2022-08-13 10:48:20 -0300

    Create salarioPorMes.py

---
+++ salarioPorMes.py
@@ -0,0 +1,6 @@
+sal_hora = float(input('digite seu salário por hora: '))
+qtd_hora = int(input('digite a quantidade de horas trabalhadas no mes: '))
+
+salario = sal_hora * qtd_hora
+
+print('seu salario é', salario, 'reais')
